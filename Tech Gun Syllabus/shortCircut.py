b176ccc4a157525e0b2fbce26b4bf385ae3d1336
Aman Raj <aman146200@gmail.com>
2024-12-22 15:52:57 +0530

    python3

--- Tech Gun Syllabus/shortCircut.py
+++ Tech Gun Syllabus/shortCircut.py
@@ -1,1 +1,11 @@
-print("sonu sinha")
+#and Operator Stops evaluating as soon as it encounters a False operand because the entire expression will be False.
+#or Operator Stops evaluating as soon as it encounters a True operand because the entire expression will be True.
+
+math = 30
+hindi = 32
+comp = 20
+
+if math >10 or hindi > 10 or comp > 30:
+    print("Eligible\n")
+else:
+    print("Not Eligible\n")
